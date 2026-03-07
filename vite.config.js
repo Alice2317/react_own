650f2a8773eb4c59c88bde4475d2b635c626a8cc
Alice <onlyrong000@gmail.com>
2026-03-07 17:11:50 +0800

    update path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/react-own/' : '/',
+    base: mode === 'production' ? '/react_own/' : '/',
     plugins: [react()],
   }
 })
